4a60f0bad1d509c8843e4b918d09a4c6273e4230
Cyz Chen <33188956+cyz1101@users.noreply.github.com>
2019-03-28 01:44:04 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<!--
+	Powered by html5up
+-->
+<html>
+	<head>
+		<title>國立高雄大學 學生權益申訴平台</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/menu.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="icon" href="images/logo_b.png">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<meta name="author" content="Cyz Chen">
+		<meta property="og:locale" content="zh_TW"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="國立高雄大學 學生權益申訴平台"/>
+		<meta property="og:description" content="在校內生活中，有事情讓你覺得權益受侵害了嗎？覺得學校處理有問題，卻不知該如何開始反應？快填寫下方表單，讓學生會給你協助吧！"/>
+		<style>
+			.error {color: #FF0000;}
+		</style>
+	</head>
+	<!-- Header -->
+			<div id="home">
+				<section id="four" class="main style2 special">
+					<div class="container">
+						<header class="major">
+							<h2>國立高雄大學 學生權益申訴平台</h2>
+							<P>在校內生活中，有事情讓你覺得權益受侵害了嗎？</br>覺得學校處理有問題，卻不知該如何開始反應？</br>快填寫下方表單，讓學生會給你協助吧！</P>
+						</header>
+					</div>
+				</section>
+			</div>
+			<div id="apply" margin: 0px auto;>
+				<section id="one" class="main style special">
+					<header class="major">
+						<h2>申訴表單</h2>
+					</header>
+					<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSemZu6pdTuP0sqlh5YlGm9Y9XgbzvJXeoC1RP941v-B2OiVhg/viewform?embedded=true" width="640" height="1236" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+				</section>
+			</div>
+			<div id="show" margin: 0px auto;>
+				<section id="four" class="main style1 special">
+					<div class="container">
+						<header class="major">
+							<h2>受理狀況</h2>
+						</header>
+						<section>
+							<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTv7_tIZvqCmfEODzLCFcT-PvVANgeiZuI_xo8qihvxGTXcBpNjVhIpJYbGgA8LXyG87sBocgwmkFwv/pubhtml?gid=495107858&amp;single=true&amp;widget=true&amp;headers=false" width="780" height="800"></iframe>
+						</section>
+					</div>
+				</section>
+			</div>
+			
+		<!-- Footer -->
+			<section id="footer">
+				<ul class="icons">
+					<li><a href="https://www.facebook.com/NUKStudent/" class="icon alt fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; 國立高雄大學學生自治會. All rights reserved.</li><li>Design: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li><li>Modify: <a href="http://cyz1101.github.io/aboutme" target="_blank">Cyz Chen</a></li>
+				</ul>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
